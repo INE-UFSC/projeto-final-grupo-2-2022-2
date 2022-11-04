3356b566a45c7ce979a9d3a28acb521348a759e6
INE-UFSC <leosteffanileoleo@gmail.com>
2022-11-03 22:41:42 -0300

    ADD mapa interativo (hitboxes não definidas)

--- prototipo/Cenario.py
+++ prototipo/Cenario.py
@@ -1,8 +1,29 @@
 from Jogo import Jogo
-
-class Cenario:
-    def __init__(self, identificador:str):
+import pygame
+class Cenario(pygame.sprite.Sprite):
+    def __init__(self, identificador:str,largura:int,altura:int,eixo_x:int,eixo_y:int,x_hit,y_hit):
         self.__id = identificador
-    
-    def imagen(self):
-        pass
+        pygame.sprite.Sprite.__init__(self)
+        self.__clicked = False
+        self.__identificador = identificador
+        self.image = pygame.image.load(f'assets/{identificador}.png').convert()
+        self.image = pygame.transform.scale(self.image,(largura, altura))
+        self.rect = self.imagem.get_rect()
+        self.rect.center = (eixo_x), (eixo_y)
+        self.__x_hit = x_hit
+        self.__y_hit = y_hit
+        self.__hitbox = pygame.Rect(x_hit, y_hit , 400, 500)
+
+    @property
+    def hitbox(self):
+        return self.__hitbox
+    @property
+    def imagem(self):
+        return self.image
+    @property
+    def clicked(self):
+        return self.__clicked
+    @clicked.setter
+    def clicked(self,novo_clicked:bool):
+        self.__clicked = novo_clicked
+
--- prototipo/Mapa.py
+++ prototipo/Mapa.py
@@ -1,11 +1,31 @@
 from Jogo import Jogo
 from Cenario import Cenario
+import pygame
 
-class Mapa:
-    def __init__(self, locais: list, caminhos:dict, posicao: Cenario):
+class Mapa(pygame.sprite.Sprite):
+    def __init__(self, locais, id_image,altura,largura,eixo_x,eixo_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.__altura = altura
+        self.__largura = largura
+        self.__eixo_x = eixo_x
+        self.__eixo_y = eixo_y
         self.__locais = locais
-        self.__posicao = posicao
-        self.__caminhos = caminhos
+        self.__id_image = id_image
+        self.image = pygame.image.load(f'assets/{id_image}.jpg').convert()
+        self.image = pygame.transform.scale(self.image, (largura, altura))
+        self.rect = self.image.get_rect()
+        self.rect.center = (eixo_x), (eixo_y)
     
-    def viagem(self, destino: Cenario):
-        pass
+    @property
+    def id_imagem(self):
+        return self.__id_image
+    @property
+    def locais(self):
+        return self.__locais
+    def escolher_local(self, destino: str):
+        local_escolhido = self.__locais[destino]
+        return local_escolhido
+
+    @property
+    def imagem(self):
+        return self.image
