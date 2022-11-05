f7f274b5abed251022eaae8e48f0c44804e13490
Tális Breda <talis.breda@outlook.com>
2022-11-04 23:10:13 -0300

    Ajustado dano da habilidade

--- prototipo/main.py
+++ prototipo/main.py
@@ -12,7 +12,7 @@ winw, winh = window.get_size()
 
 # Elementos a serem mostrados na tela
 acoes = [
-    Acao('fireball', -50, 'saude', 'ofensivo')
+    Acao('fireball', -5, 'saude', 'ofensivo')
 ]
 
 mago = Mago('aaaa', 5, 100, acoes)
@@ -110,32 +110,6 @@ def main(window: pygame.Surface):
                 
 
         update_window(window)
-
-            # if not attack:
-            #     rand = Random()
-            #     targetPos = rand.randint(0, 2)
-            #     target = enemies[targetPos]
-            #     x, y = char.rect.centerx - 10, char.rect.centery-10
-            #     fireball = PersonagemView(window, 'fireball.png', x, y, 20, 20)
-            #     team.append(fireball)
-            #     attack = True
-            # else:
-            #     fireball.rect.x += x_speed
-            #     match targetPos:
-            #         case 0:
-            #             pass
-            #         case 1:
-            #             fireball.rect.y -= x_speed/5
-            #         case 2:
-            #             fireball.rect.y += x_speed/5
-
-            #     if fireball.rect.collidepoint(target.rect.center):
-            #         attack = False
-            #         team.remove(fireball)
-            #         print("char2 -5 vida")
-            
-
-
 
 if __name__ == "__main__":
     main(window)
