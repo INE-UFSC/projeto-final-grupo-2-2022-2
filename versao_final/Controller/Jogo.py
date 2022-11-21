aec9bac15e779f2802c1f64f4cfe52538fb815a9
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2022-11-21 19:56:28 -0300

    inclusão dos orcs

--- versao_final/Controller/Jogo.py
+++ versao_final/Controller/Jogo.py
@@ -9,63 +9,61 @@ import pygame
 class Jogo:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((900, 500),
+        self.__window = pygame.display.set_mode((900, 500),
                                               pygame.RESIZABLE)
-        self.winw, self.winh = self.window.get_size()
+        self.__winw, self.__winh = self.__window.get_size()
 
         # Elementos a serem mostrados na tela
-        self.acoes = [
+        self.__acoes = [
             Acao('fireball', -5, 'saude', 'ofensivo'),
             Acao('boost', 5, 'ataque', 'suporte')
         ]
 
-        self.elements = ['']*7
+        self.__elements = ['']*7
         for i in range(7):
-            self.elements[i] = pygame.transform.scale(pygame.image.load(
+            self.__elements[i] = pygame.transform.scale(pygame.image.load(
                                                       os.path.join('assets',
                                                       'retangulo.png')),
                                                       (50, 50))
 
-        self.magos = ['']*6
-        for i in range(6):
-            self.magos[i] = Personagem('Joao', 5,
-                                       100, self.acoes,
-                                       'mago', (70, 80))
-
-        self.time = ['']*3
-        self.inimigos = ['']*3
+        self.__magos = ['']*3
+        self.__orcs = ['']*3
+        self.__time = ['']*3
+        self.__inimigos = ['']*3
         for i in range(3):
-            self.time[i] = PersonagemView(self.window,
-                                          self.magos[i],
-                                          100, 200)
-            #PersonagemView(window, magos[4], 200, 100),
-            #PersonagemView(window, magos[5], 200, 300)
-            self.inimigos[i] =  PersonagemView(self.window,
-                                               self.magos[i+3],
-                                               700, 200)
-            #PersonagemView(window, magos[2], 600, 100),
-            #PersonagemView(window, magos[3], 600, 300)
+            self.__magos[i] = Personagem('Joao' + str(i), 10,
+                                       100, self.__acoes,
+                                       'mago', (70, 80))
+            self.__orcs[i] = Personagem('Mateus' + str(i), 10,
+                                       100, self.__acoes,
+                                       'orc', (70, 80))
+            self.__time[i] = PersonagemView(self.__window,
+                                          self.__magos[i],
+                                          200, 100 + i*100)
+            self.__inimigos[i] = PersonagemView(self.__window,
+                                          self.__orcs[i],
+                                          600, 100 + i*100)
 
-        self.resultado = []
+        self.__resultado = []
 
-        self.jogo = BatalhaController(self.time,
-                                      self.inimigos)
+        self.__jogo = BatalhaController(self.__time,
+                                      self.__inimigos)
         #if __name__ == "__main__":
-        #    self.main()
+        #    self.__main()
 
     def update_window(self):
-        self.window.fill((255, 255, 255))
-        for PersonagemView in self.time:
+        self.__window.fill((255, 255, 255))
+        for PersonagemView in self.__time:
             PersonagemView.draw()
-        for PersonagemView in self.inimigos:
+        for PersonagemView in self.__inimigos:
             PersonagemView.draw()
         cont = 7
-        for elemento in self.elements:
-            self.window.blit(elemento, (self.winw/22*cont,
-                                        self.winh-50))
+        for elemento in self.__elements:
+            self.__window.blit(elemento, (self.__winw/22*cont,
+                                        self.__winh-50))
             cont += 1
-        for i in self.resultado:
-            self.window.blit(i, (self.winw/4, self.winh/4))
+        for i in self.__resultado:
+            self.__window.blit(i, (self.__winw/4, self.__winh/4))
         pygame.display.update()
         
 
@@ -79,14 +77,14 @@ class Jogo:
         while run:
             clock.tick(fps)
             for event in pygame.event.get():
-                finished = self.jogo.watch(event, finished)
+                finished = self.__jogo.watch(event, finished)
                 if event.type == pygame.QUIT:
                     return False
                 if finished:
                     result = pygame.transform.scale(
                         pygame.image.load(os.path.join('assets',
                                                        f'allies.png')),
-                        (self.winw/2, self.winh/2))
-                    self.resultado.append(result)
+                        (self.__winw/2, self.__winh/2))
+                    self.__resultado.append(result)
 
             self.update_window()
--- versao_final/main.py
+++ versao_final/main.py
@@ -4,12 +4,12 @@ jogo = Jogo()
 jogo.main()
 
 '''
-from Model.Personagem import Personagem
-from Model.Batalha import Batalha
-from Model.Acao import Acao
+from combatClasses.mago import Mago
+from Batalha import Batalha
+from Acao import Acao
 import os
-from Controller.BatalhaController import BatalhaController
-from View.PersonagemView import PersonagemView
+from BatalhaController import BatalhaController
+from PersonagemView import PersonagemView
 import pygame
 
 pygame.init()
@@ -18,31 +18,65 @@ winw, winh = window.get_size()
 
 # Elementos a serem mostrados na tela
 acoes = [
-    Acao('fireball', -5, 'saude', 'ofensivo'),
-    Acao('boost', 5, 'ataque', 'suporte')
+    Acao('fireball', -5, 'saude', 'ofensivo')
 ]
 
-elements = ['']*7
-for i in range(7):
-    elements[i] = pygame.transform.scale(
+mago = Mago('aaaa', 5, 100, acoes)
+mago1 = Mago('aaaa', 5, 100, acoes)
+mago2 = Mago('aaaa', 5, 100, acoes)
+mago3 = Mago('aaaa', 5, 100, acoes)
+mago4 = Mago('aaaa', 5, 100, acoes)
+mago5 = Mago('aaaa', 5, 100, acoes)
+retangulo = pygame.transform.scale(
+    pygame.image.load(os.path.join('assets', 'retangulo.png')),
+    (50, 50))
+retangulo2 = pygame.transform.scale(
+    pygame.image.load(os.path.join('assets', 'retangulo.png')),
+    (50, 50))
+retangulo3 = pygame.transform.scale(
+    pygame.image.load(os.path.join('assets', 'retangulo.png')),
+    (50, 50))
+retangulo4 = pygame.transform.scale(
+    pygame.image.load(os.path.join('assets', 'retangulo.png')),
+    (50, 50))
+retangulo5 = pygame.transform.scale(
+    pygame.image.load(os.path.join('assets', 'retangulo.png')),
+    (50, 50))
+retangulo6 = pygame.transform.scale(
+    pygame.image.load(os.path.join('assets', 'retangulo.png')),
+    (50, 50))
+retangulo7 = pygame.transform.scale(
     pygame.image.load(os.path.join('assets', 'retangulo.png')),
     (50, 50))
 
-magos = ['']*6
-for i in range(6):
-   magos[i] = Personagem('Joao', 5, 100, acoes, 'mago', (70, 80))
 
-time = ['']*3
-inimigos = ['']*3
-for i in range(3):
-    time[i] = PersonagemView(window, magos[0], 100, 200)
-    #PersonagemView(window, magos[4], 200, 100),
-    #PersonagemView(window, magos[5], 200, 300)
-    inimigos[i] =  PersonagemView(window, magos[1], 700, 200)
-    #PersonagemView(window, magos[2], 600, 100),
-    #PersonagemView(window, magos[3], 600, 300)
 
-resultado = []
+# Lista de Elementos
+time = [
+    PersonagemView(window, mago, 100, 200),
+    PersonagemView(window, mago4, 200, 100),
+    PersonagemView(window, mago5, 200, 300)
+]
+
+inimigos = [
+    PersonagemView(window, mago1, 700, 200),
+    PersonagemView(window, mago2, 600, 100),
+    PersonagemView(window, mago3, 600, 300)
+]
+
+elements = [
+    retangulo,
+    retangulo2,
+    retangulo3,
+    retangulo4,
+    retangulo5,
+    retangulo6,
+    retangulo7,
+]
+
+resultado = [
+
+]
 
 jogo = BatalhaController(time, inimigos)
 
@@ -53,7 +87,7 @@ def update_window(window:pygame.Surface):
     for PersonagemView in inimigos:
         PersonagemView.draw()
     cont = 7
-    for elemento in elements:
+    for i, elemento in enumerate(elements):
         window.blit(elemento, (winw/22*cont, winh-50))
         cont += 1
     for i in resultado:
