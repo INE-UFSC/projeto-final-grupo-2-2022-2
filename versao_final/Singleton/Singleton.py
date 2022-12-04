28d8cd5e19f4b57e2778989cb7572b2ee7156f76
talisbreda <talis.breda@outlook.com>
2022-12-04 00:26:05 -0300

    Create nova classe Singleton

---
+++ versao_final/Singleton/Singleton.py
@@ -0,0 +1,25 @@
+from Model.Acao import Acao
+
+class Singleton:
+    __instance = None
+    __screenSize = (1200, 600)
+    __skills = [
+        Acao('fireball', -200, 'saude', 'ofensivo'),
+        Acao('boost', 5, 'ataque', 'suporte')
+        ]
+
+    def __new__(cls, *args):
+        if cls.__instance is None:
+            cls.__instance = object.__new__(cls, *args)
+        return cls.__instance
+    
+    @property
+    def screenSize(cls):
+        return cls.__screenSize
+    @screenSize.setter
+    def screenSize(cls, size):
+        cls.__screenSize = size
+
+    @property
+    def skills(cls):
+        return cls.__skills
