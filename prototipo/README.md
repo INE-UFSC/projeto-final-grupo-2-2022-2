b18021e1a1e65aa5dd602aff7b92e54d15804136
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2022-12-09 21:43:49 -0300

    Mais ajustes

--- prototipo/README.md
+++
@@ -1,5 +0,0 @@
-O arquivo principal, que apresenta a mecânica principal do jogo, é o main.py. Esse arquivo é o que executa uma batalha entre duas equipes.
-
-O arquivo menu.py executa o menu do jogo, que atualmente está incompleto. O botão "play" abre o mapa do jogo, que possui só uma localidade funcional. Futuramente, um clique em um local do mapa abrirá uma batalha. O botão quit fecha a aplicação.
-
-Não é necessário instalar nenhuma biblioteca além do pygame.
--- versao_final/Model/CenarioBatalha.py
+++ versao_final/Model/CenarioBatalha.py
@@ -1,13 +1,18 @@
 from View.Cenario import Cenario
 
 class CenarioBatalha(Cenario):
-    def __init__(self,identificador:str, largura:int,
+    def __init__(self, identificador:str, largura:int,
                  altura:int, eixo_x:int, eixo_y:int,
                  nivel:int, inimigos:list):
         super().__init__(identificador, largura,
                          altura, eixo_x, eixo_y)
         self.__inimigos = inimigos
+        self.__nivel = nivel
 
-        @property
-        def inimigos(self) -> list:
-            return self.__inimigos
+    @property
+    def nivel(self) -> list:
+        return self.__nivel
+
+    @property
+    def inimigos(self) -> list:
+        return self.__inimigos
