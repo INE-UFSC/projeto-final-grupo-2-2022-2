525217ded92d5b03787388557190a2f801acbb80
maykonmarcosjunior <112183802+maykonmarcosjunior@users.noreply.github.com>
2022-12-15 09:04:15 -0300

    Correção de Caminho

--- versao_final/DAO/JogoDAO.py
+++ versao_final/DAO/JogoDAO.py
@@ -1,9 +1,8 @@
-from Controller.DAO import DAO
+from DAO.DAO import DAO
 
 class JogoDAO(DAO):
     def __init__(self, datasource='Nivel.pkl'):
         super().__init__(datasource)
-        self.add(0)
 
     def add(self, nivel:int):
         if isinstance(nivel, int):
