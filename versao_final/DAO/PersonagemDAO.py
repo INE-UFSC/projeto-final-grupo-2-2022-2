c7e6fc21e6c9073da4bd9ee246815192c731061c
maykonmarcosjunior <112183802+maykonmarcosjunior@users.noreply.github.com>
2022-12-15 09:05:55 -0300

    Transferência de Nova Estrutura

--- versao_final/DAO/PersonagemDAO.py
+++ versao_final/DAO/PersonagemDAO.py
@@ -1,6 +1,6 @@
 from DAO.DAO import DAO
 from Model.Personagem import Personagem
-from Model.Acao import Acao
+from Model.Habilidade import Habilidade
 from copy import deepcopy
 
 class PersonagemDAO(DAO):
@@ -12,39 +12,41 @@ class PersonagemDAO(DAO):
         cond = cond and isinstance(char.nome, str)
         cond = cond and isinstance(char.classe, str)
         cond = cond and isinstance(char.nivel, int)
-        cond = cond and isinstance(char.ataque_max, int)
+        cond = cond and isinstance(char.ataque, int)
         cond = cond and isinstance(char.saude_max, int)
-        cond = cond and isinstance(char.tecnicas, list)
+        cond = cond and isinstance(char.habilidades, list)
         tecnicas = {}
         while cond:
-            for i in char.tecnicas:
-                cond = cond and isinstance(i, Acao)
+            for i in char.habilidades:
+                cond = cond and isinstance(i, Habilidade)
                 cond = cond and isinstance(i.nome, str)
                 cond = cond and isinstance(i.fator, int)
+                cond = cond and isinstance(i.efeito, int)
+                cond = cond and isinstance(i.tipo, str)
                 tecnicas[i.nome] = [i.nome,
                                  i.fator,
                                  i.efeito,
-                                 i.tipo,
-                                 i.modo]
-            valor = [char.ataque_max,
+                                 i.tipo]
+            valor = [char.ataque,
                     char.saude_max,
                     char.nivel,
                     char.classe,
+                    char.posicao,
                     tecnicas]
             super().add(char.nome, valor)
 
     def get(self, nome:str) -> Personagem:
         j = deepcopy(self.__temp_get(nome))
         tecnicas = self.__get_tecnicas(nome)
-        personagem = Personagem(nome, j[2], tecnicas, j[3])
-        personagem.save_ataque_max(j[0], 'pode usar')
-        personagem.save_saude_max(j[1], 'pode usar')
+        personagem = Personagem(j[3], j[2], tecnicas, j[4], nome)
+        personagem.save_ataque(j[0], 'ataque_autorizado')
+        personagem.save_saude_max(j[1], 'vida_autorizada')
         return personagem
 
     def remove(self, key:str):
         if isinstance(key, str):
             return super().remove(key)
-        self.get_tecnicas
+        self.__get_tecnicas
     
     def get_all(self) -> list[Personagem]:
         temp_dicio = deepcopy(super().get_all())
@@ -62,12 +64,12 @@ class PersonagemDAO(DAO):
 
 # transforma os atributos de tecnicas salvas em Acao
 
-    def __get_tecnicas(self, nome:str) -> list[Acao]:
+    def __get_tecnicas(self, nome:str) -> list[Habilidade]:
         char = deepcopy(self.__temp_get(nome))
-        temp_tecnicas = deepcopy(char[4])
+        temp_tecnicas = deepcopy(char[5])
         tecnicas = ['']*len(temp_tecnicas)
         j = 0
         for i in temp_tecnicas.values():
-            tecnicas[j] = Acao(i[0], i[1], i[2], i[3], i[4])
+            tecnicas[j] = Habilidade(i[0], i[1], i[2], i[3])
             j += 1
         return tecnicas
